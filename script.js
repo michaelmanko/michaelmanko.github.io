d635266ae5244f7d610cd29fec102cabe5b3d5c4
Michael Manko <michaelmanko21@gmail.com>
2024-04-09 20:12:01 -0400

    c

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("home").style.display = "block"; // Display Home page by default
-
-function showPage(pageId) {
-  var pages = document.getElementsByClassName("page");
-  for (var i = 0; i < pages.length; i++) {
-    pages[i].style.display = "none";
-  }
-  document.getElementById(pageId).style.display = "block";
-}
